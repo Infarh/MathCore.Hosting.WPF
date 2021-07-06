9c6050b556a278569ec890b6fb673bb027f7f771
Infarh <shmachilin@yandex.ru>
2021-07-06 18:23:07 +0300

    Добавлено свойство для доступа к провайдеру сервисов и к конфигурации приложения

--- MathCore.Hosting.WPF/ApplicationHosting.cs
+++ MathCore.Hosting.WPF/ApplicationHosting.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,16 +10,33 @@ namespace MathCore.Hosting.WPF
 {
     public abstract class ApplicationHosting : Application
     {
+        public static Window? FocusedWindow => Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsFocused);
+        public static Window? ActiveWindow => Current.Windows.Cast<Window>().FirstOrDefault(w => w.IsActive);
+        public static Window? CurrentWindow => FocusedWindow ?? ActiveWindow ?? Current.MainWindow;
+
         private static IHost? __Hosting;
 
         public static IHost Hosting => __Hosting ??= CreateHostBuilder(Environment.GetCommandLineArgs()).Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] Args) => Host.CreateDefaultBuilder(Args)
-           .ConfigureServices((h, s) => (Current as ApplicationHosting)?.ConfigureServices(h, s));
+        public static IServiceProvider Services => Hosting.Services;
+
+        public static IConfiguration Configuration => Services.GetRequiredService<IConfiguration>();
+
+        public static IHostBuilder CreateHostBuilder(string[] Args)
+        {
+            var builder = Host.CreateDefaultBuilder(Args);
+            var app = Current as ApplicationHosting;
+            builder = app?.ConfigureHostBuilder(builder) ?? builder;
+            builder.ConfigureServices((h, s) => (Current as ApplicationHosting)?.ConfigureServices(h, s));
+            return app?.ConfigureHostBuilderFinal(builder) ?? builder;
+        }
+
+        protected virtual IHostBuilder ConfigureHostBuilder(IHostBuilder builder) => builder;
+
+        protected virtual IHostBuilder ConfigureHostBuilderFinal(IHostBuilder builder) => builder;
 
         protected virtual void ConfigureServices(HostBuilderContext host, IServiceCollection services) => 
             services.AddServicesFromAssembly(GetType());
-
 
         protected override async void OnStartup(StartupEventArgs e)
         {
---
+++ MathCore.Hosting.WPF/HostedServiceLocator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MathCore.Hosting.WPF
+{
+    public class ServiceLocatorHosted : ServiceLocator
+    {
+        protected override IServiceProvider Services => ApplicationHosting.Services;
+    }
+}
