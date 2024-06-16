884f00293421e4e7ab78ecc4bdb37d17bad57970
Павел <shmachilin@gmail.com>
2024-06-17 01:10:36 +0300

    v0.0.13 - Добавлен метод-расширение для MathCore.WPF.Command, обеспечивающий возможностьб логирования процесса выполнения

---
+++ MathCore.Hosting.WPF/Extensions/CommandEx.cs
@@ -0,0 +1,31 @@
+﻿using MathCore.WPF.Commands;
+using Microsoft.Extensions.Logging;
+
+namespace MathCore.Hosting.WPF.Extensions;
+
+public static class CommandEx
+{
+    public static TCommand WithLogging<TCommand>(this TCommand command, ILogger logger) where TCommand : Command
+    {
+        command.BeforeExecuted += BeforeCommandExecuting;
+        command.Executed += OnCommandExecuted;
+        command.Error += OnCommandExecutingError;
+
+        return command;
+
+        void BeforeCommandExecuting(object? Sender, EventArgs<object?> E)
+        {
+            logger.LogInformation("Command {0} start executing with parameter {1}", command, E.Argument);
+        }
+
+        void OnCommandExecuted(object? Sender, EventArgs<object?> E)
+        {
+            logger.LogInformation("Command {0} executed successful with parameter {1}", command, E.Argument);
+        }
+
+        void OnCommandExecutingError(object Sender, ExceptionEventHandlerArgs<Exception> Args)
+        {
+            logger.LogError("Command {0} thrown error {1}", command, Args.Argument);
+        }
+    }
+}
